11983df08bc2bad27b717dcd305d6a6d43881f03
Sans000ye <vuonghuuquocbao@gmail.com>
2025-02-24 14:57:57 +0700

    restructuring

--- ClothingWebsite/clothingwebsite.client/src/main.jsx
+++ ClothingWebsite/clothingwebsite.client/src/main.jsx
@@ -1,7 +1,8 @@
+//Imports
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Component/App'
+import App from './Component/Testing/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
