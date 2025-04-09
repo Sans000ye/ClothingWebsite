89d5d299ac634290d465270f38d0c32b9453fd56
slh4003 <tamquytran1244@gmail.com>
2025-04-09 19:03:02 +0700

    Login_ASP

---
+++ AuthAPI/AuthAPI/DTOs/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthAPI.DTOs
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ AuthAPI/AuthAPI/Program.cs
@@ -0,0 +1,44 @@
+using AuthAPI.Data;
+using AuthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("ReactFrontend");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
