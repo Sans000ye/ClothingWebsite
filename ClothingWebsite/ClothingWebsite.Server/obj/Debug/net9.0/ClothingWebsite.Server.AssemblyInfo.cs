566b2716a54a4318b07f8c6055873955fbfdd2fa
Sans000ye <vuonghuuquocbao@gmail.com>
2025-04-16 15:56:26 +0700

    update

--- ClothingWebsite/ClothingWebsite.Server/obj/Debug/net9.0/ClothingWebsite.Server.AssemblyInfo.cs
+++ ClothingWebsite/ClothingWebsite.Server/obj/Debug/net9.0/ClothingWebsite.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingWebsite.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece360a7c06507fefb3c5b5994ffbbdfec0c82c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d3c31af854b09d89b3e939217dcbc905c50677")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingWebsite.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingWebsite.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
